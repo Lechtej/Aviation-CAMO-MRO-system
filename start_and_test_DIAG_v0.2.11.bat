9f4a55290c2f7f83bac08cef0e7c4cf18bd1bf28
Leszek Tejwan <leszek.tejwan@gmail.com>
2026-01-06 14:30:34 +0100

    v 0.2.27 2

--- start_and_test_DIAG_v0.2.11.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set "DIAG=1"
-call "%~dp0start_and_test_v0.2.11.bat"
-exit /b %errorlevel%
--- start_and_test_DIAG_v0.2.12.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set "DIAG=1"
-call "%~dp0start_and_test_v0.2.12.bat"
-exit /b %errorlevel%
